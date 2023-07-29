46ef5a91899331212eb83791da0071507f414999
vladimir-bogdanovic <vladimirbogdanovic44@gmail.com>
2023-07-29 17:32:13 +0200

    form in add-new-meal component - style and validators

--- restaurant/src/app/add-new-meal/add-new-meal.component.ts
+++ restaurant/src/app/add-new-meal/add-new-meal.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 import { Location } from '@angular/common';
 import { DataSharingService } from '../shared/data-sharing.service';
@@ -9,29 +9,33 @@ import { DataSharingService } from '../shared/data-sharing.service';
   templateUrl: './add-new-meal.component.html',
   styleUrls: ['./add-new-meal.component.css'],
 })
-export class AddNewMealComponent {
+export class AddNewMealComponent implements OnInit {
   mealToAdd: string = 'key';
+  addNewMealForm: FormGroup;
 
   constructor(
     private location: Location,
     private dataSharingService: DataSharingService
   ) {}
 
-  addNewMealForm = new FormGroup({
-    strMeal: new FormControl(),
-    strMealThumb: new FormControl(),
-    strIngredient1: new FormControl(),
-    strIngredient2: new FormControl(),
-    strIngredient3: new FormControl(),
-    strIngredient4: new FormControl(),
-    strIngredient5: new FormControl(),
-    strIngredient6: new FormControl(),
-    strIngredient7: new FormControl(),
-    strIngredient8: new FormControl(),
-    strIngredient9: new FormControl(),
-  });
+  ngOnInit(): void {
+    this.addNewMealForm = new FormGroup({
+      strMeal: new FormControl(null, Validators.required),
+      strMealThumb: new FormControl(),
+      strIngredient1: new FormControl(null, Validators.required),
+      strIngredient2: new FormControl(),
+      strIngredient3: new FormControl(),
+      strIngredient4: new FormControl(),
+      strIngredient5: new FormControl(),
+      strIngredient6: new FormControl(),
+      strIngredient7: new FormControl(),
+      strIngredient8: new FormControl(),
+      strIngredient9: new FormControl(),
+    });
+  }
 
   onSubmit() {
+    console.log(this.addNewMealForm);
     this.dataSharingService.sendData(this.addNewMealForm.value);
     this.location.back();
     localStorage.setItem(
