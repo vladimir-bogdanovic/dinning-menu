fa3d98dc69c56bcc533d19a27609896f497ef97b
vladimir-bogdanovic <vladimirbogdanovic44@gmail.com>
2023-08-16 15:50:24 +0200

    eeeeeeelp!!!

--- restaurant/src/app/menu-page/category-meals/category-meals.component.ts
+++ restaurant/src/app/menu-page/category-meals/category-meals.component.ts
@@ -41,13 +41,15 @@ export class CategoryMealsComponent implements OnInit {
 
   isEditing: boolean = false;
   editButtonClicked: boolean = false;
-  editMealForm!: FormGroup;
+  dynamicFormName!: FormGroup;
 
   newMealInfo: SingleMealDetailsInterface | null;
   addedItemInfo!: SingleMealDetailsInterface;
 
-  sastojci: (string | null)[] = [];
-  sastojciRandomJela: (string | null)[] = [];
+  ingredients: (string | null)[] = [];
+  randomMealIngredients: (string | null)[] = [];
+  dynamicForm!: FormGroup;
+  ingredientValue: any;
 
   constructor(
     private callingService: CallingService,
@@ -119,7 +121,7 @@ export class CategoryMealsComponent implements OnInit {
         this.randomMeal = resData.meals[0];
         Object.entries(this.randomMeal).map((data) => {
           if (data[0].includes('strIngredient') && data[1] !== '') {
-            this.sastojciRandomJela.push(data[1]);
+            this.randomMealIngredients.push(data[1]);
           }
         });
       });
@@ -135,12 +137,10 @@ export class CategoryMealsComponent implements OnInit {
     this.mealSelected = true;
     if (!id) {
       this.selectedMealDetail = this.addedItemInfo;
-      this.sastojci.length = 0;
-      console.log(this.selectedMealDetail);
-      console.log(this.addedItemInfo);
+      this.ingredients.length = 0;
       Object.entries(this.selectedMealDetail).map((data: string[]) => {
         if (data[0].includes('strIngredient') && data[1] !== '') {
-          this.sastojci.push(data[1]);
+          this.ingredients.push(data[1]);
         }
       });
     } else {
@@ -148,10 +148,10 @@ export class CategoryMealsComponent implements OnInit {
         .getMealDetails(id)
         .subscribe((resData: mealsDetailsInterface) => {
           this.selectedMealDetail = resData.meals[0];
-          this.sastojci.length = 0;
+          this.ingredients.length = 0;
           Object.entries(this.selectedMealDetail).map((data: string[]) => {
             if (data[0].includes('strIngredient') && data[1] !== '') {
-              this.sastojci.push(data[1]);
+              this.ingredients.push(data[1]);
             }
           });
         });
@@ -167,73 +167,35 @@ export class CategoryMealsComponent implements OnInit {
   editMeal(meal: SingleMealDetailsInterface) {
     this.isEditing = true;
     this.editButtonClicked = true;
+    this.dynamicForm = this.formBuilder.group({});
 
-    // this.editMealForm = new FormGroup({
-    //   strMeal: new FormControl(meal.strMeal),
-    //   strIngredient1: new FormControl(meal.strIngredient1),
-    //   strIngredient2: new FormControl(meal.strIngredient2),
-    //   strIngredient3: new FormControl(meal.strIngredient3),
-    //   strIngredient4: new FormControl(meal.strIngredient4),
-    //   strIngredient5: new FormControl(meal.strIngredient5),
-    //   strIngredient6: new FormControl(meal.strIngredient6),
-    //   strIngredient7: new FormControl(meal.strIngredient7),
-    //   strIngredient8: new FormControl(meal.strIngredient8),
-    //   strIngredient9: new FormControl(meal.strIngredient9),
-    // });
-
-    this.editMealForm = this.formBuilder.group({
-      strMeal: new FormControl(meal.strMeal),
-      // strMeal: [null],
-      strIngredient1: [null],
-      strIngredient2: [null],
-      strIngredient3: [null],
-      strIngredient4: [null],
-      strIngredient5: [null],
-      strIngredient6: [null],
-      strIngredient7: [null],
-      strIngredient8: [null],
-      strIngredient9: [null],
-      strIngredient10: [null],
-      strIngredient11: [null],
-      strIngredient12: [null],
-      strIngredient13: [null],
-      strIngredient14: [null],
-      strIngredient15: [null],
-      strIngredient16: [null],
-      strIngredient17: [null],
-      strIngredient18: [null],
-      strIngredient19: [null],
-      strIngredient20: [null],
+    Object.entries(meal).map((data: string[]) => {
+      if (
+        data[0].includes('strIngredient') &&
+        data[1] !== '' &&
+        data[1] !== undefined &&
+        data[1] !== null
+      ) {
+        this.dynamicForm.addControl(data[0], new FormControl(data[1]));
+      }
     });
-
-    this.editMealForm.patchValue(this.selectedMealDetail);
   }
   saveMealChanges() {
-    this.selectedMealDetail.strMeal = this.editMealForm.value.strMeal;
-    this.selectedMealDetail.strIngredient1 =
-      this.editMealForm.value.strIngredient1;
-    this.selectedMealDetail.strIngredient2 =
-      this.editMealForm.value.strIngredient2;
-    this.selectedMealDetail.strIngredient3 =
-      this.editMealForm.value.strIngredient3;
-    this.selectedMealDetail.strIngredient4 =
-      this.editMealForm.value.strIngredient4;
-    this.selectedMealDetail.strIngredient5 =
-      this.editMealForm.value.strIngredient5;
-    this.selectedMealDetail.strIngredient6 =
-      this.editMealForm.value.strIngredient6;
-    this.selectedMealDetail.strIngredient7 =
-      this.editMealForm.value.strIngredient7;
-    this.selectedMealDetail.strIngredient8 =
-      this.editMealForm.value.strIngredient8;
-    this.selectedMealDetail.strIngredient9 =
-      this.editMealForm.value.strIngredient9;
     this.editButtonClicked = false;
     this.isEditing = false;
   }
   cancelMealChanges() {
     this.isEditing = false;
     this.editButtonClicked = false;
+  }
+
+  editSpecificIngredient(i) {
+    Object.entries(this.dynamicForm.value).map((data) => {
+      console.log(typeof +data[0].slice(13));
+      if (+data[0].slice(13) === i + 1) {
+        console.log(data[1]);
+      }
+    });
   }
   // EDITING MEAL END
 }
